1e5b547594e9c3df993f4eff45e35bb8a888ddf4
khatba <khatba.husain@gmail.com>
2020-03-21 13:47:52 +0100

    add flash msg

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -15,21 +15,22 @@ class RentsController < ApplicationController
   end
 
   def create
-    
-    puts 'create' * 100
-    puts params
-    puts 'create' * 100
-    
     Rent.create(prix: params[:prix], bike_id: params[:bike_id], payed: false, date_disponible: params[:date_disponible], date_end: params[:date_end])
+    flash[:success] = 'Vous avez ajouter une location !'
+    redirect_to rents_path
   end
 
   def update
     Rent.find(params[:id]).update(user_id: current_user.id)
+    flash[:success] = 'Vous avez ajouter une location à votre cart !'
+    redirect_to user_path(current_user.id)
   end
 
   def destroy
     puts "destroy" * 100
     Rent.find(params[:id]).update(user_id: nil)
+    flash[:success] = 'Vous avez supprimer une location de votre cart !'
+    redirect_to rents_path
   end
 
 
