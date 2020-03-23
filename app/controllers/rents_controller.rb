232efb539fbd47714b1ac05cdc272880a0d35050
khatba <khatba.husain@gmail.com>
2020-03-23 10:33:24 +0100

    add ajax

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -15,10 +15,10 @@ class RentsController < ApplicationController
   end
 
   def create
-
     @rent = Rent.new(prix: params[:prix], bike_id: params[:bike_id], payed: false, date_disponible: params[:date_disponible], date_end: params[:date_end])
     if @rent.save 
       flash[:success] = 'Vous avez bien ajouter une location !'
+      redirect_to rents_path
     else
     flash[:error] = 'Veuillez vérifier la date ou le prix !'
     redirect_to new_rent_path
@@ -33,18 +33,12 @@ class RentsController < ApplicationController
     if (@rent.user_id == nil)
       @rent.update(user_id: current_user.id)
       flash[:success] = 'Vous avez ajouter une location à votre cart !'
-      respond_to do |format|
-        format.html { redirect_to rents_path }
-        format.js { } 
-      end
+      redirect_to rents_path
       
     elsif (@rent.user_id == current_user.id)
       @rent.update(user_id: nil)
       flash[:success] = 'Vous avez suprimer une location à votre cart !'
-      respond_to do |format|
-        format.html { redirect_to rents_path }
-        format.js { } 
-      end
+      redirect_to rents_path
     end
 
 
@@ -52,7 +46,8 @@ class RentsController < ApplicationController
 
 
   def destroy
-
+    Rent.find(params[:id]).update(user_id: nil)
+    redirect_to rents_path
   end
 
 
