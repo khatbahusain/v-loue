11d6dea206ecaf3850f1a1a27455263cd5d12a34
khatba <khatba.husain@gmail.com>
2020-03-27 17:35:54 +0100

    fix city problem

--- db/migrate/20200327054732_create_table_cities.rb
+++
@@ -1,8 +0,0 @@
-class CreateTableCities < ActiveRecord::Migration[6.0]
-  def change
-    create_table :table_cities do |t|
-      t.string   :name,        null: false
-      t.string   :code_postal,   null: false
-    end
-  end
-end
--- db/migrate/20200327065517_create_cities.rb
+++
@@ -1,9 +0,0 @@
-class CreateCities < ActiveRecord::Migration[6.0]
-  def change
-    change_table :cities do |t|
-      t.string   :name,        null: false
-      t.string   :code_postal,   null: false
-
-    end
-  end
-end
